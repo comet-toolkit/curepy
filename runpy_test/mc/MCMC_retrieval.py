9df41460caff547ece58e79cc7dae23abf198368
Pieter De Vis <pieter.de.vis@npl.co.uk>
2021-10-20 13:51:00 +0100

    changes for using ancillary data vector b

--- runpy_test/mc/MCMC_retrieval.py
+++ runpy_test/mc/MCMC_retrieval.py
@@ -70,7 +70,7 @@ class MCMCRetrieval:
         return self.measurement_function(*xb)
 
     def make_x_tuple(self,theta):
-        x=[guess.asfloat() for guess in self.initial_guess]
+        x=[guess.astype(float) for guess in self.initial_guess]
         j=0
         for i in range(len(x)):
             if not hasattr(x[i],'__len__'):
