8583c4de864f6afb4701a5052d776b16f196eaa2
Pieter De Vis <pieter.de.vis@npl.co.uk>
2021-11-29 20:01:29 +0000

    adding capability to reuse samples of b

--- runpy_test/mc/MCMC_retrieval.py
+++ runpy_test/mc/MCMC_retrieval.py
@@ -165,7 +165,7 @@ class MCMCRetrieval:
 
             self.b = b[:]
             if include_b_results:
-                samples_comb = np.empty(((nwalkers*steps-burn_in)*self.b_iter,len(theta_0))+len(self.b),
+                samples_comb = np.empty(((nwalkers*steps-burn_in)*self.b_iter,len(theta_0)+len(self.b)),
                                    dtype=np.ndarray)
                 samples_comb[:,::len(theta_0)] = samples
                 samples_comb[:,len(theta_0)::] = b_samples
