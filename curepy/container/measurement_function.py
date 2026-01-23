92cf6c782e2a4a275a8ebc504f0ab6c21095e969
Joe <joe.riordan@npl.co.uk>
2026-01-23 15:40:32 +0000

    MeasurementFunction class structure

---
+++ curepy/container/measurement_function.py
@@ -0,0 +1,25 @@
+"""Container for Measurement Function"""
+
+from typing import Union, List
+
+class MeasurementFunction:
+    def __init__(self,
+                 measurement_func,
+                 initial_guess = None,
+                 measurand_name: str = None,
+                 input_quantities_names: Union[str, List[str]] = None,
+                 ):
+        
+        self.measurement_func = measurement_func
+        self._measurand_name = measurand_name
+        self._input_quantities_names = input_quantities_names
+
+        self.initial_guess = self.format_initial_guess(initial_guess)
+        
+    @staticmethod
+    def _format_initial_guess(initial_guess):
+        raise NotImplementedError
+    
+    def measurement_function_x():
+        raise NotImplementedError
+    #is this general enough to be here?
