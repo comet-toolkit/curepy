4583917ce8a39da3cab29f89fa6cd2620eb94ee1
Joe <joe.riordan@npl.co.uk>
2026-01-28 16:02:29 +0000

    prior class

--- curepy/container/prior.py
+++ curepy/container/prior.py
@@ -2,12 +2,25 @@
 
 import numpy as np
 
+implemented_prior_shapes = ["uniform",
+                            ]
+
 class Prior:
     def __init__(self,
         shape: str = None,
         params: dict = {}
     ):
-        self.shape = shape
         
+        self.implemented_prior_shapes = implemented_prior_shapes
+        
+        if shape.lower() in self.implemented_prior_shapes:
+            self.shape = shape
+        else:
+            raise ValueError(
+                f"The provided prior shape ({shape}) is not an implemented prior distribution shape {self.implemented_prior_shapes}"
+            )
+        
+        
+    
         
         
