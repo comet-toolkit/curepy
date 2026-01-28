9c759966397afa0b4ab801132bc897543b7ab496
Joe <joe.riordan@npl.co.uk>
2026-01-28 15:22:44 +0000

    refactor base for new input class

--- curepy/retrieval_methods/base.py
+++ curepy/retrieval_methods/base.py
@@ -1,23 +1,21 @@
 """Base class for retrieval methods"""
 
 from abc import ABC, abstractmethod
-from curepy.container.measurement_function import MeasurementFunction
-from curepy.container.ancillary_parameter import AncillaryParameter
-from curepy.container.prior import Prior
-from curepy.container.measurement import Measurement
+from curepy.container.retrieval_input import RetrievalInput
 
 class BaseRetrieval(ABC):
     """Base retrieval object"""
     
-    def __init__(
-        self,
-    parallel_cores = 1,
+    def __init__(self, 
+                 parallel_cores = 1,
     ):
+        
         self.parallel_cores = parallel_cores
     
     @abstractmethod
-    def run_retrieval(self):
+    def run_retrieval(self,
+                      retrieval_inputs: RetrievalInput):
         pass
-    
+
 if __name__ == "__main__":
     pass
