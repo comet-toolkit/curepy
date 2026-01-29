ecbc24deb2486cf165f1368b391c025b4a653412
Joe <joe.riordan@npl.co.uk>
2026-01-29 17:05:18 +0000

    ln_uniform distribution

--- curepy/utilities/distributions.py
+++ curepy/utilities/distributions.py
@@ -1,2 +1,14 @@
 """Distribution functions"""
 
+import numpy as np
+
+def ln_uniform(theta, minimum, maximum):
+    if np.all(
+        minimum.flatten() < theta
+        ) and np.all(
+        maximum.flatten() > theta
+    ):
+            return 0
+        
+    else:
+        return -np.inf
