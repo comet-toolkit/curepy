1f050a3374765d6069122bee2c4e6c3b3be5bc8c
Joe <joe.riordan@npl.co.uk>
2026-01-29 17:05:06 +0000

    prior ln_function implementation

--- curepy/container/prior.py
+++ curepy/container/prior.py
@@ -1,8 +1,10 @@
 """Container for Prior information"""
 
 import numpy as np
+from curepy.utilities.distributions import *
 
-implemented_prior_shapes = {"uniform": [],#todo: populate dict
+implemented_prior_shapes = {"uniform": {"function": ln_uniform,
+                                        "params": ["minimum", "maximum"]}
                             }
 
 class Prior:
@@ -12,7 +14,8 @@ class Prior:
     ):
         
         self._check_inputs(prior_shape, prior_params)
-        #todo: add function to initialise prior function
+        self.ln_prior = implemented_prior_shapes[prior_shape]["function"]
+        self.prior_params = prior_params
             
     def _check_inputs(shape, params):
         #check shape
@@ -21,14 +24,34 @@ class Prior:
                 f"The provided prior shape ({shape}) is not an implemented prior distribution shape {implemented_prior_shapes.keys()}"
             )
         #check required params exist 
-        if all(params.keys()) in implemented_prior_shapes[shape]:# todo: fix check and possibly add optional param check
+        if set(params.keys()) == set(implemented_prior_shapes[shape]["params"]):# todo: fix check and possibly add optional param check
             raise ValueError(
                 f"Prior shape ({shape}) requires the following inputs in param dictionary {implemented_prior_shapes[shape]}"
             )
         
         
         
-        
+    def lnlike(self, theta):
+        # print(theta,self.find_chisum(theta))
+        return -0.5 * (self.find_chisum(theta))
+
+    def lnprior(self, theta):
+        if np.all(self.downlims.flatten() < theta) and np.all(
+            self.uplims.flatten() > theta
+        ):
+            # if self.syst_uncertainty[0] is None:
+            return 0
+        # else:
+        #     return -0.5*(theta[0]**2/self.syst_uncertainty**2)
+        else:
+            return -np.inf
+
+    def lnprob(self, theta):
+        lp_prior = self.lnprior(theta)
+        if not np.isfinite(lp_prior):
+            return -np.inf
+        lp = self.lnlike(theta)
+        return lp_prior + lp
     
         
         
