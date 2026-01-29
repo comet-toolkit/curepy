aa7fe05079ebaaae5503c818ae8cc37787035132
Joe <joe.riordan@npl.co.uk>
2026-01-29 16:08:32 +0000

    individual build functions

--- curepy/container/retrieval_input.py
+++ curepy/container/retrieval_input.py
@@ -58,6 +58,47 @@ class RetrievalInput:
         self.prior_obj = Prior(prior_shape,
                                prior_params)
         
+    def build_measurement_function(self,
+                                   measurement_func,
+                                    initial_guess,
+                                    multiple_guess_measurements: bool = False,
+                                    measurand_name: str = None,
+                                    input_quantities_names: Union[str, List[str]] = None,):
         
+        self.measurement_function_obj = MeasurementFunction(measurement_func,
+                 initial_guess,
+                 multiple_guess_measurements,
+                 measurand_name,
+                 input_quantities_names)
         
+    def build_measurement(self,
+                          y,
+                          u_y = None,
+                          corr_y = None,):
         
+        self.measurement_obj = Measurement(y, 
+                                           u_y,
+                                           corr_y)
+        
+    def build_prior(self,
+                    prior_shape: str = None,
+                    prior_params: dict = {},):
+        
+        self.prior_obj = Prior(prior_shape,
+                               prior_params)
+        
+    def build_ancillary(self,
+                        b = None,
+                        u_b = None,
+                        corr_b = None,
+                        corr_between_b = None,
+                        b_samples = None,
+                        b_iter = None,
+                        ):
+        
+        self.ancilary_obj = AncillaryParameter(b,
+                                               u_b,
+                                               corr_b,
+                                               corr_between_b,
+                                               b_samples,
+                                               b_iter)
