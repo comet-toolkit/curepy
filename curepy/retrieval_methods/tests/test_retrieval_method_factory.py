8c91779287bc89fc55ee118da41b4545d18fcd67
Joe <joe.riordan@npl.co.uk>
2026-01-29 15:49:11 +0000

    fix tests, mistyped input

--- curepy/retrieval_methods/tests/test_retrieval_method_factory.py
+++ curepy/retrieval_methods/tests/test_retrieval_method_factory.py
@@ -7,10 +7,10 @@ from curepy.retrieval_methods.retrieval_method_factory import RetrievalFactory
 class TestRetrievalFactory(unittest.TestCase):
     def test_get_brdf_model_valid_name(self):
         factory = RetrievalFactory()
-        model = factory.make_retrieval_object("mcmc", nwalkers = 100, burnin = 100, steps = 1000)
+        model = factory.make_retrieval_object("mcmc", nwalkers = 100, burn_in = 100, steps = 1000)
         self.assertEqual(model.__class__.__name__, "MCMC")
 
     def test_get_brdf_model_invalid_name(self):
         factory = RetrievalFactory()
         with self.assertRaises(ValueError):
-            model = factory.make_retrieval_object("invalid", nwalkers = 100, burnin = 100, steps = 1000)
+            model = factory.make_retrieval_object("invalid", nwalkers = 100, burn_in = 100, steps = 1000)
