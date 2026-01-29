6c63fb3060d2e61d33be29892371cc7511931b36
Joe <joe.riordan@npl.co.uk>
2026-01-29 16:00:22 +0000

    refactor prior class

--- curepy/container/prior.py
+++ curepy/container/prior.py
@@ -2,8 +2,8 @@
 
 import numpy as np
 
-implemented_prior_shapes = ["uniform",
-                            ]
+implemented_prior_shapes = {"uniform": [],#todo: populate dict
+                            }
 
 class Prior:
     def __init__(self,
@@ -11,14 +11,22 @@ class Prior:
         params: dict = {}
     ):
         
-        self.implemented_prior_shapes = implemented_prior_shapes
-        
-        if shape.lower() in self.implemented_prior_shapes:
-            self.shape = shape
-        else:
+        self._check_inputs(shape, params)
+        #todo: add function to initialise prior function
+            
+    def _check_inputs(shape, params):
+        #check shape
+        if shape.lower() not in implemented_prior_shapes:
             raise ValueError(
-                f"The provided prior shape ({shape}) is not an implemented prior distribution shape {self.implemented_prior_shapes}"
+                f"The provided prior shape ({shape}) is not an implemented prior distribution shape {implemented_prior_shapes.keys()}"
             )
+        #check required params exist 
+        if all(params.keys()) in implemented_prior_shapes[shape]:# todo: fix check and possibly add optional param check
+            raise ValueError(
+                f"Prior shape ({shape}) requires the following inputs in param dictionary {implemented_prior_shapes[shape]}"
+            )
+        
+        
         
         
     
