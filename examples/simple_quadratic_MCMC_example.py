c89220c8c10bfe4e7195ea4e2767c2f887806cc2
Joe <joe.riordan@npl.co.uk>
2026-01-30 10:43:02 +0000

    add plotting utilities and edit example

--- examples/simple_quadratic_MCMC_example.py
+++ examples/simple_quadratic_MCMC_example.py
@@ -4,6 +4,7 @@ from curepy.container.ancillary_parameter import AncillaryParameter
 from curepy.container.prior import Prior
 from curepy.retrieval_methods.MCMC import MCMC
 from curepy.container.retrieval_input import RetrievalInput
+from curepy.utilities.plotting import corner
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -24,10 +25,14 @@ inputs = RetrievalInput(meas_func, meas, ancill)
 
 ret = MCMC(100, 1000, 100)
 
-results = ret.run_retrieval(inputs)
+results = ret.run_retrieval(inputs, return_samples=True)
 
 print(results.values)
 print(results.uncertainties)
 plt.plot(x, quadratic(*results.values, x))
 plt.scatter(x, y, alpha = 0.5, c = 'orange')
 plt.savefig("C:/Users/jr20/code/curepy/examples/test.png")
+
+corner = corner(results.samples,
+                labels = ['a', 'b', 'c'])
+corner.savefig("C:/Users/jr20/code/curepy/examples/test_corner.png")
