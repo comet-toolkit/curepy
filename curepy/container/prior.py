a09fb03cd26085125cc97c33dbe3569291687eab
Joe <joe.riordan@npl.co.uk>
2026-02-02 14:46:46 +0000

    remove completed todo

--- curepy/container/prior.py
+++ curepy/container/prior.py
@@ -25,7 +25,7 @@ class Prior:
                 f"The provided prior shape ({shape}) is not an implemented prior distribution shape {implemented_prior_shapes.keys()}"
             )
         #check required params exist 
-        if set(params.keys()) != set(implemented_prior_shapes[shape]["params"]):# todo: fix check and possibly add optional param check
+        if set(params.keys()) != set(implemented_prior_shapes[shape]["params"]):
             raise ValueError(
                 f"Prior shape ({shape}) requires the following inputs in param dictionary {implemented_prior_shapes[shape]}"
             )
