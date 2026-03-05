afec080801436f5660c7aa1fe0f768edffaacca0
Joe <joe.riordan@npl.co.uk>
2026-03-05 15:33:26 +0000

    tests for format initial guess

--- curepy/container/tests/test_measurement_function.py
+++ curepy/container/tests/test_measurement_function.py
@@ -18,3 +18,24 @@ class TestMeasurementFunction(unittest.TestCase):
         out = mf.measurement_function(mf.initial_guess, 
                                       6)
         npt.assert_array_equal(out, np.array([11,16]))
+        
+    def test__format_initial_guess_scalar(self):
+        initial_guess = 5
+        formatted_guess = MeasurementFunction._format_initial_guess(initial_guess)
+        npt.assert_array_equal(formatted_guess, np.array([5]))
+        
+    def test__format_initial_guess_list(self):
+        initial_guess = [5, 6, 8]
+        formatted_guess = MeasurementFunction._format_initial_guess(initial_guess)
+        npt.assert_array_equal(formatted_guess, np.array([5, 6, 8]))
+        
+    def test__format_initial_guess_array(self):
+        initial_guess = np.ones((3, 2))
+        formatted_guess = MeasurementFunction._format_initial_guess(initial_guess)
+        npt.assert_array_equal(formatted_guess, np.array([[1,1], [1,1], [1,1]]))
+        
+    def test__format_initial_guess_ragged_array(self):
+        initial_guess = [np.array([2, 4]), 6]
+        formatted_guess = MeasurementFunction._format_initial_guess(initial_guess)
+        npt.assert_array_equal(formatted_guess[0], np.array([2,4]))
+        npt.assert_array_equal(formatted_guess[1], np.array([6]))
