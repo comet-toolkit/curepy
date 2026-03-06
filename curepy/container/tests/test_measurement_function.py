68fb3fdfb5dc1edade90c2ec172ed1ccb2f2a65a
Joe <joe.riordan@npl.co.uk>
2026-03-06 11:12:37 +0000

    tests for measurementFunction

--- curepy/container/tests/test_measurement_function.py
+++ curepy/container/tests/test_measurement_function.py
@@ -9,6 +9,9 @@ from curepy.container.measurement_function import MeasurementFunction
 
 def dummy_function(x, b):
     return x + b
+
+def dummy_function_2(x, b1, b2):
+    return x + b1 - b2
     
 class TestMeasurementFunction(unittest.TestCase):
     def test_init_builds_basic(self):
@@ -39,3 +42,64 @@ class TestMeasurementFunction(unittest.TestCase):
         formatted_guess = MeasurementFunction._format_initial_guess(initial_guess)
         npt.assert_array_equal(formatted_guess[0], np.array([2,4]))
         npt.assert_array_equal(formatted_guess[1], np.array([6]))
+
+    def test_make_x_tuple_list(self):
+        initial_guess = [0.0, 0.0, 0.0]
+        mf = MeasurementFunction(dummy_function, initial_guess)
+        out = mf.make_x_tuple([10, 20, 30]) 
+        self.assertEqual(out, (10, 20, 30))
+
+    def test_make_x_tuple_1d_array(self):
+        initial_guess = [0.0, 0.0, 0.0]
+        mf = MeasurementFunction(dummy_function, initial_guess)
+        out = mf.make_x_tuple(np.array([10, 20, 30])) 
+        self.assertEqual(out, (10, 20, 30))
+
+    def test_make_x_tuple_ragged_list(self):
+        initial_guess = [[0, 0], [0]]
+        mf = MeasurementFunction(dummy_function, initial_guess)
+        out = mf.make_x_tuple(np.array([10, 20, 30])) 
+        npt.assert_array_equal(out[0], np.array([10,20]))
+        self.assertEqual(out[1], np.array([30]))
+
+    def test__make_x_tuple_ragged_numpy_array(self):
+        initial_guess = np.array([np.array([0, 0]), np.array([0])], dtype=object)
+        mf = MeasurementFunction(dummy_function, initial_guess)
+        out = mf.make_x_tuple([9, 8, 7])
+        assert isinstance(out, tuple) and len(out) == 2
+        assert isinstance(out[0], np.ndarray) and isinstance(out[1], np.ndarray)
+        assert np.array_equal(out[0], np.array([9, 8]))
+        assert np.array_equal(out[1], np.array([7]))
+
+    def test_make_x_tuple_too_deep_nesting_raises(self):
+        initial_guess = [[[[0]]]]
+        mf = MeasurementFunction(dummy_function, initial_guess)
+        with self.assertRaises(ValueError):
+            mf.make_x_tuple([1])
+
+    def test_make_x_tuple_theta_too_short_raises(self):
+        initial_guess = [0, 0, 0]
+        mf = MeasurementFunction(dummy_function, initial_guess)
+        with self.assertRaises(IndexError):
+            mf.make_x_tuple([1, 2])
+            
+    def test_measurement_function_x(self):
+        theta = np.array([2,3,4])
+        b = [1]
+        mf = MeasurementFunction(dummy_function, [[0,0,0]])
+        out = mf.measurement_function_x(theta, b)
+        npt.assert_array_equal(out, np.array([3,4,5]))
+        
+    def test_measurement_function_flattened_output(self):
+        theta = np.array([2,3,4,5])
+        b = [1]
+        mf = MeasurementFunction(dummy_function, [[[0,0], [0,0]]])
+        out = mf.measurement_function_flattened_output(theta, b)
+        npt.assert_array_equal(out, np.array([3,4,5,6]))
+        
+    def test_measurement_function_flattened_b(self):
+        theta = [6]
+        b_flat = np.array([1,2,3])
+        mf = MeasurementFunction(dummy_function_2, [0])
+        out = mf.measurement_function_flattened_b(theta, b_flat, [(2,),(1,)])
+        npt.assert_array_equal(out, np.array([4,5]))
