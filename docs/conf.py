3672c222e1ba9943811a8035ae27a051085fc2dd
Joe <joe.riordan@npl.co.uk>
2026-03-11 11:50:05 +0000

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -140,6 +140,10 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_logo = "curepy_logo.png"
+html_title = "curepy"
+
+
 # SH added to override wide tables in RTD theme
 html_context = {
     "css_files": [
