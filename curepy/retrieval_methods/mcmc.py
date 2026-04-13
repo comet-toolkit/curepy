ecfdca90bf8ba7d1818ef82e37d7f02d2634aab8
Joe <joe.riordan@npl.co.uk>
2026-04-13 12:01:51 +0100

    bug fix for correlation calculation along one dimension

--- curepy/retrieval_methods/mcmc.py
+++ curepy/retrieval_methods/mcmc.py
@@ -224,6 +224,8 @@ class MCMC(BaseRetrieval):
         unc_avg = (unc_up + unc_down) / 2.0
 
         if return_corr:
+            if corr_dims != -99:
+                samples.reshape((samples.shape[0],) + self.retrieval_input.measurement_function_obj.initial_guess.shape)
             corr = cm.calculate_corr(samples, corr_dims)
 
         if reshape_results:
